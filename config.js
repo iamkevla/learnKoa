cdc02792864bb697a1fe453d11720cc7da037e3f
Kevin Vlahos <kevin.vlahos@gmail.com>
2015-06-29 16:32:03 +1000

    more updates

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    koa: {
+        port: 3000,
+				"https-port": 3001
+    }
+}
--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 'use strict';
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+var koa = require('koa');
+
 var forceSSL = require('koa-force-ssl');
 var serve = require('koa-static');
-var koa = require('koa');
 var fs = require('fs');
 var https = require('https');
+var router = require('koa-router')();
+var proxy = require('koa-http-proxy')
+
+// Load config koa
+var config = require(__dirname + "/config.js");
 
 var app = koa();
 
-app.use(forceSSL());
+app.use(forceSSL(config.koa['https-port']));
 
 // or use absolute paths
 app.use(serve(__dirname + '/public'));
 
-app.listen('3000');
+
+app.use(router.routes(app));
+
+router.get('/api/v1/sessions', proxy({
+	target: 'https://dealerportaltest/api/v1/users/login',
+	secure: false
+}));
+
+app.listen(config.koa.port);
+console.log('Listening on port ' + config.koa.port);
 
 // SSL options
 var options = {
-  key: fs.readFileSync('private/www.commander.com.key'),
-  cert: fs.readFileSync('private/www.commander.com.cert')
+    key: fs.readFileSync('private/www.commander.com.key'),
+    cert: fs.readFileSync('private/www.commander.com.cert')
 }
 
-https.createServer(options, app.callback()).listen('443');
+https.createServer(options, app.callback()).listen(config.koa['https-port']);
+console.log('Listening on port ' + config.koa['https-port']);
