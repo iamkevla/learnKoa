460a49d16772f07707261ac68b69fb0402a699af
Kevin Vlahos <kevin.vlahos@gmail.com>
2015-06-30 11:34:19 +1000

    no real improvement

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
+var debug = require('debug')('learnkoa');
+
 var koa = require('koa');
 
 var forceSSL = require('koa-force-ssl');
@@ -9,24 +11,29 @@ var serve = require('koa-static');
 var fs = require('fs');
 var https = require('https');
 var router = require('koa-router')();
-var proxy = require('koa-http-proxy')
+var proxy = require('koa-http-proxy');
 
 // Load config koa
-var config = require(__dirname + "/config.js");
+var config = require(__dirname + '/config.js');
 
 var app = koa();
 
-app.use(forceSSL());
+app.use(forceSSL('443', 'localhost', true));
+debug('redirect', '3001');
 
 // or use absolute paths
 app.use(serve(__dirname + '/public'));
 
 
-if (!module.parent) app.use(router.routes(app));
+if (!module.parent) app
+  .use(router.routes(app))
+  .use(router.allowedMethods());
+
 
 router.get('/api/v1/sessions', proxy({
-	target: 'https://dealerportaltest/api/v1/users/login',
-	secure: false
+  target: 'https://dealerportaltest/',
+  forward: 'api/v1/users/login',
+  secure: false
 }));
 
 app.listen(config.koa.port);
@@ -34,9 +41,9 @@ console.log('Listening on port ' + config.koa.port);
 
 // SSL options
 var options = {
-    key: fs.readFileSync('private/privatekey.pem'),
-    cert: fs.readFileSync('private/certificate.pem')
-}
+  key: fs.readFileSync('private/privatekey.pem', 'utf8'),
+  cert: fs.readFileSync('private/certificate.pem', 'utf8')
+};
 
 https.createServer(options, app.callback()).listen(443);
 console.log('Listening on port ' + '443');
